dc91fd57598ab099795d23aaef89fb123224782d
cabreraem1 <52495686+cabreraem1@users.noreply.github.com>
2019-09-21 13:02:49 -0400

    Add project files.

---
+++ ClassFundamentals/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassFundamentals.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            calculator.Add(1, 2);
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+    }
+    
+    
+}
